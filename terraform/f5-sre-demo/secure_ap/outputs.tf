c5f2302c7f776d23722d3924664f55d84bebf901
Michael Kennedy <merpso@gmail.com>
2020-05-27 08:54:45 +1000

    updated to remove big-iq

--- terraform/f5-sre-demo/secure_ap/outputs.tf
+++ terraform/f5-sre-demo/secure_ap/outputs.tf
@@ -32,7 +32,7 @@ output "jumphost_ip" {
   description = "ip address of jump host"
   value       = module.jumphost.jumphost_ip
 }
-
+/*
 output "juiceshop_ip" {
   value = module.jumphost.juiceshop_ips[*].public_ip
 }
@@ -40,7 +40,7 @@ output "juiceshop_ip" {
 output "grafana_ip" {
   value = module.jumphost.grafana_ips[*].public_ip
 }
-
+*/
 # Instance Information
 output "ec2_key_name" {
   description = "the key used to communication with ec2 instances"
